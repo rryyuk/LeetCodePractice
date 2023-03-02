6862f1b8834e157c588ad5bb31bba0b0dc23a3eb
rryyuk <agranshul15@gmail.com>
2023-03-02 23:31:19 +0530

    March 2

---
+++ March_1/QInsertInLinkedListAtAPosition.java
@@ -0,0 +1,43 @@
+package March_1;
+
+public class QInsertInLinkedListAtAPosition {
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode() {}
+        public ListNode(int val) { this.val = val; }
+        public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public static void main(String[] args){
+        ListNode c = new ListNode(4,null);
+        ListNode b = new ListNode(3,c);
+        ListNode a = new ListNode(2,b);
+        ListNode head = new ListNode(1,a);
+        ListNode tempNode = head;
+        ListNode nextNode = new ListNode();
+
+        int n = 2;
+        int i = 1;
+
+        if(head == null){
+            System.out.println("null");
+        } else if ( n == 1){
+            head = tempNode.next;
+            tempNode = null;
+        } else {
+            while(i < n -1) {
+                tempNode = tempNode.next;
+                i++;
+            }
+            nextNode = tempNode.next;
+            tempNode.next = nextNode.next;
+            nextNode = null;
+
+
+        }
+        System.out.println(head.val);
+        System.out.println(head.next.val);
+        System.out.println(head.next.next.val);
+        System.out.println(head.next.next.next.val);
+    }
+}
---
+++ March_2/Q15Q3Sum.java
@@ -0,0 +1,4 @@
+package March_2;
+
+public class Q15Q3Sum {
+}
