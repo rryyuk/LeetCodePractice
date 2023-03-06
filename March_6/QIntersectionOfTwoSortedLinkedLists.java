839bef865412cbe58bb07e42a05fbe26c0ba2914
rryyuk <agranshul15@gmail.com>
2023-03-06 22:48:54 +0530

    March 6

---
+++ March_6/QIntersectionOfTwoSortedLinkedLists.java
@@ -0,0 +1,59 @@
+package March_6;
+
+
+import java.net.SocketOption;
+
+public class QIntersectionOfTwoSortedLinkedLists {
+
+
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode() {}
+        public ListNode(int val) { this.val = val; }
+        public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode front ;
+    public static ListNode rear ;
+
+    public static void enqueue(int data) {
+        ListNode insertingData = new ListNode(data);
+        if(front == null) {
+            front = rear = insertingData;
+        }
+        rear.next = insertingData;
+        rear = insertingData;
+
+    }
+    public static int dequeue() {
+        if(front == null) {
+            System.out.println("Empty queue");
+        }
+        int element = front.val;
+        front = front.next;
+        return element;
+    }
+
+    public static void display() {
+        ListNode temp = front;
+        if(temp == null) {
+            System.out.println("Empty queue");
+        } else {
+            while(temp != null) {
+                System.out.print(temp.val +" ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        enqueue(1);
+        enqueue(2);
+        enqueue(3);
+        enqueue(4);
+        display();
+        dequeue();
+        display();
+    }
+}
