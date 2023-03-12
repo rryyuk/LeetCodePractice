d6159efd1018b788853f48fbdb0e37f9af25d073
rryyuk <agranshul15@gmail.com>
2023-03-13 00:00:58 +0530

    March 12

--- March_2/Q15Q3Sum.java
+++ March_2/Q15Q3Sum.java
@@ -1,4 +1,38 @@
 package March_2;
 
+import java.util.Arrays;
+
 public class Q15Q3Sum {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int low = i + 1;
+                int high = nums.length - 1;
+                int sum = 0 - nums[i];
+
+                while (low < high) {
+                    if (nums[low] + nums[high] == sum) {
+                        System.out.println(nums[i] + " " + nums[low] + " " + nums[high]);
+                        while (low < high && nums[low] == nums[low + 1]) {
+                            low++;
+                        }
+                        while (low < high && nums[low] == nums[low + 1]) {
+                            high--;
+                        }
+
+                        low++;
+                        high--;
+                    } else if (nums[low] + nums[high] < sum) {
+                        low++;
+                    } else {
+                        high--;
+                    }
+                }
+            }
+
+        }
+    }
 }
