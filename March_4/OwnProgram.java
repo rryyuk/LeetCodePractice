e10f7d8441ced5e822da79bb51bb8a61bb006817
rryyuk <agranshul15@gmail.com>
2023-03-22 22:45:08 +0530

    March 19

--- March_4/OwnProgram.java
+++ March_4/OwnProgram.java
@@ -32,45 +32,41 @@ public class OwnProgram {
             }
         }
     }
-    public static void insert(Node root,int value) {
+    public static void insert(Node root,int value, int target, boolean isInserted) {
 
-        if(root.left == null && root.right == null) {
-            root.left = new Node(value);
-        }
-        if(root.left == null) {
-            root.left = new Node(value);
-        }
-        if(root.right == null) {
-            root.right = new Node(value);
+        if (root != null) {
+            if(root.val == target) {
+                if(root.left == null){
+                    root.left = new Node(value);
+                } else if (root.right == null){
+                    root.right = new Node(value);
+                } else {
+                    System.out.println("No empty space");
+                }
+                isInserted = true;
+            }
         }
 
-        insert(root.left,value);
-        insert(root.right,value);
+        if(root != null) {
+            insert(root.left,value, target, isInserted);
+            if(!isInserted){
+                insert(root.right,value, target,isInserted);
+            }
+        }
+
+
     }
-    public static Node callInsert(Node root, int value)  {
-        if(root == null) {
-            return new Node(value);
-        } else {
-            return insert(root,value);
-        }
-        return null;
-    }
 
-    public static void delete(Node root, int val) {
-        if(root.val == val) {
-
-        }
-    }
     public static void main(String[] args) {
         Node a = new Node(1);
-        Node b = new Node();
         a.left = new Node(2);
         a.right = new Node(3);
         a.left.left = new Node(4);
         a.left.right = new Node(5);
-        Node c = callInsert(b,5);
 
-        print(insert(b,5));
+        insert(a,10,3,false);
+        print(a);
+
 
     }
 }
