cb8729a483a43d45288fc6eedbcd1fa4f1920726
rryyuk <75989838+rryyuk@users.noreply.github.com>
2023-04-16 19:31:35 +0530

    March 9

---
+++ March 9/Q160IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+
+        if(headA == null || headB == null) {
+            return null;
+        }
+
+        while(temp1 != temp2) {
+            if(temp1 == null) {
+                temp1 = headB;
+            } else {
+                temp1 = temp1.next;
+            }
+            if(temp2 == null) {
+                temp2 = headA;
+            } else {
+                temp2 = temp2.next;
+            }
+        }
+        return temp1;
+    }
+}
