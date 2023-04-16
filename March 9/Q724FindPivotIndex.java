5b5970e82e9d6234ebf205deff1b699b866dcb7f
Anshul Agrawal <75989838+rryyuk@users.noreply.github.com>
2023-04-17 00:08:55 +0530

    March 9

---
+++ March 9/Q724FindPivotIndex.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSumOfArray = 0;
+        for(int i = 0; i< nums.length; i++) {
+            totalSumOfArray = totalSumOfArray + nums[i];
+        }
+
+        int leftElementSum = 0;
+
+        for(int i = 0; i< nums.length; i++ ) {
+            if( i!= 0) {
+                leftElementSum = leftElementSum + nums[i-1];
+            }
+            if(totalSumOfArray - leftElementSum - nums[i] == leftElementSum ) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
