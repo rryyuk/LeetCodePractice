d151a825c2dac32551aa4e1341972fef6102154a
Anshul Agrawal <75989838+rryyuk@users.noreply.github.com>
2023-04-21 22:45:26 +0530

    March_15

---
+++ March_17/Q104MaximumDepthOfBinaryTree.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return depthFind(root);
+    }
+
+    public int depthFind(TreeNode root) {
+        if(root == null) {
+            return 0; 
+        }
+
+        int leftMax = depthFind(root.left);
+        int rightMax = depthFind(root.right);
+
+        return Math.max(leftMax,rightMax) +1;
+    }
+}
