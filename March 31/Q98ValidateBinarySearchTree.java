fee8a9ad8bfc14eab36fb8e94a838a47cc655e67
Anshul Agrawal <75989838+rryyuk@users.noreply.github.com>
2023-04-26 02:03:37 +0530

    March 31

---
+++ March 31/Q98ValidateBinarySearchTree.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+
+    public boolean isValid(TreeNode root, long minVal,long maxVal) {
+        if(root == null) {
+            return true;
+        } 
+        if(root.val >= maxVal || root.val <= minVal) {
+            return false;
+        }
+
+        return isValid(root.left, minVal, root.val) && isValid(root.right, root.val, maxVal);
+    }
+}
