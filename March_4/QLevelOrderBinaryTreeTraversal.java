2c890acc3502f603a72394b0e5a53ea87932a361
Anshul Agrawal <75989838+rryyuk@users.noreply.github.com>
2023-05-14 21:42:11 +0530

    March 4

--- March_4/QLevelOrderBinaryTreeTraversal.java
+++ March_4/QLevelOrderBinaryTreeTraversal.java
@@ -7,7 +7,7 @@ public class QLevelOrderBinaryTreeTraversal {
     public static class Node {
         int val;
         Node left;
-        Node right;
+        Node right; 
 
         Node() {
         }
