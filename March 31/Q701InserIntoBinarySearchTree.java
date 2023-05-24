c7e5c18f1b5ecbc5ba3edcee0dd51eb9d761ed76
Anshul Agrawal <75989838+rryyuk@users.noreply.github.com>
2023-05-24 23:56:36 +0530

    March 31

--- March 31/Q701InserIntoBinarySearchTree.java
+++ March 31/Q701InserIntoBinarySearchTree.java
@@ -4,7 +4,7 @@ class Solution {
             return new TreeNode(val);
         } else if( root.val < val) {
             root.right = insertIntoBST(root.right, val); 
-        } else if (root.val > val) {
+        } else if (root.val > val) { 
             root.left = insertIntoBST(root.left, val);
         } 
         return root;
