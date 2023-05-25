b600893b62a6bb39e2ec11d7fb7bc7eb28bf55fe
Anshul Agrawal <75989838+rryyuk@users.noreply.github.com>
2023-05-25 22:42:59 +0530

    March 9

--- March 9/Q55JumpGame.java
+++ March 9/Q55JumpGame.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean canJump(int[] nums) {
         int index = nums.length - 1;
         
-        for(int i = nums.length - 1; i>=0; i--) {
+        for(int i = nums.length - 1; i>=0; i--) { 
             if(nums[i] + i >= index) { 
                 index = i;
             }
