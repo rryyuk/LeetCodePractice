beeb89412fea00a94fab810e449e501e01dcaf45
Anshul Agrawal <75989838+rryyuk@users.noreply.github.com>
2023-05-27 22:13:20 +0530

    March 17

--- March_17/Q100SameTree.java
+++ March_17/Q100SameTree.java
@@ -4,6 +4,6 @@ class Solution {
             return (p==q);
         }
 
-        return((p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));  
+        return((p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
     }
 }
