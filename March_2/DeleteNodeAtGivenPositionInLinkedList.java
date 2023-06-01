66cea953da84497e5759130b5577b6e1808c639c
Anshul Agrawal <75989838+rryyuk@users.noreply.github.com>
2023-06-01 22:12:49 +0530

    March 2

--- March_2/DeleteNodeAtGivenPositionInLinkedList.java
+++ March_2/DeleteNodeAtGivenPositionInLinkedList.java
@@ -7,7 +7,7 @@ public class DeleteNodeAtGivenPositionInLinkedList {
         public ListNode next;
         public ListNode() {}
         public ListNode(int val) { this.val = val; }
-        public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        public ListNode(int val, ListNode next) { this.val = val; this.next = next; } 
     } 
     public static void main(String[] args){ 
 
